e39e89f354946c842b090f92714488fee25a0445
KimoAnany <info.anany@yahoo.com>
2023-02-26 22:25:26 +0100

    Data of Project added

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace SL_Abstandssimluator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDistanz = new System.Windows.Forms.Label();
+            this.txtboxDistanz = new System.Windows.Forms.TextBox();
+            this.btnAuswerten = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDistanz
+            // 
+            this.lblDistanz.AutoSize = true;
+            this.lblDistanz.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblDistanz.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDistanz.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblDistanz.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDistanz.Location = new System.Drawing.Point(128, 249);
+            this.lblDistanz.Name = "lblDistanz";
+            this.lblDistanz.Size = new System.Drawing.Size(350, 57);
+            this.lblDistanz.TabIndex = 0;
+            this.lblDistanz.Text = "Distanz [in CM]";
+            this.lblDistanz.Click += new System.EventHandler(this.lblDistanz_Click);
+            // 
+            // txtboxDistanz
+            // 
+            this.txtboxDistanz.Location = new System.Drawing.Point(678, 249);
+            this.txtboxDistanz.Multiline = true;
+            this.txtboxDistanz.Name = "txtboxDistanz";
+            this.txtboxDistanz.Size = new System.Drawing.Size(369, 57);
+            this.txtboxDistanz.TabIndex = 2;
+            this.txtboxDistanz.TextChanged += new System.EventHandler(this.txtboxDistanz_TextChanged);
+            // 
+            // btnAuswerten
+            // 
+            this.btnAuswerten.Location = new System.Drawing.Point(471, 587);
+            this.btnAuswerten.Name = "btnAuswerten";
+            this.btnAuswerten.Size = new System.Drawing.Size(369, 91);
+            this.btnAuswerten.TabIndex = 4;
+            this.btnAuswerten.Text = "Auswerten";
+            this.btnAuswerten.UseVisualStyleBackColor = true;
+            this.btnAuswerten.Click += new System.EventHandler(this.btnAuswerten_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 855);
+            this.Controls.Add(this.btnAuswerten);
+            this.Controls.Add(this.txtboxDistanz);
+            this.Controls.Add(this.lblDistanz);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Abstandscheck";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDistanz;
+        private System.Windows.Forms.TextBox txtboxDistanz;
+        private System.Windows.Forms.Button btnAuswerten;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using SmartLightLibrary;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SL_Abstandssimluator
+{
+    public partial class Form1 : Form
+    {
+        SmartLight smartLight;
+        public Form1()
+        {
+            InitializeComponent();
+            smartLight = new SmartLight();
+            smartLight.VirtualSmartLight = true;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtboxDistanz_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAuswerten_Click(object sender, EventArgs e)
+        {
+            //Deklaration name für Connection mit IDM
+            string name = "Student001";
+
+            //Verbindung zum Server mit meiner persönlichen IDM
+            smartLight.ConnectionAddress = name;
+            
+            //Befehl für 100 % Helligkeit
+            int brightness = Convert.ToInt32("100");
+
+            // Deklaration der benötigten Variablen zum Schleife bilden:
+            
+            int Distanz = Convert.ToInt32(txtboxDistanz.Text);
+
+          
+            // Grau Phase, WENN Distanz >>echt größer<< 150
+
+            if (Distanz >= 150)
+            {
+
+                smartLight.SetLedRGB(0, 0, 0);
+
+            }
+            // Grün Phase, WENN Distanz >>größer gleich<< 100 UND >>echt kleiner<< 150
+            if (Distanz >= 100 && Distanz < 150 )
+            {
+               
+            smartLight.SetLedRGB(0, 255, 0);
+
+            }
+
+
+            // Gelb Phase; WENN Distanz >>echt kleiner<< als 100 UND >>größer gleich<< 45 
+            if (Distanz < 100 && Distanz >= 45)
+            {
+
+                smartLight.SetLedRGB(255, 255, 0);
+
+            }
+
+            // Rote Phase; Wenn Distanz >>echt kleiner<< 45, DANN 
+            if(Distanz < 45)
+            {
+
+                smartLight.SetLedRGB(255, 0, 0);
+
+            }
+            /* 
+             Aus zeittechnischen Gründen, belasse ich es dabei einfach nur Zahlen einzutragen, 
+             normalerweise müsste ein zusätzlicher Testrun sowie Requirement und Testitem hinzugefügt werden 
+             für das Eintragen von Buchstaben anstelle von Zahlen und daraus folgend die Fehlereingrenzung durch entsprechenden Programmcode.
+             Dessen bin ich mir voll auf bewusst.
+             */ 
+            
+
+        }
+
+        private void txtboxIDMADRESSE_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDistanz_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
